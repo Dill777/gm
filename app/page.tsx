9a90dcf4efd05116b3d5ae187f9773e8df06563a
IceDev528 <iceprince.L528@gmail.com>
2025-08-05 16:15:29 -0400

    update sidebar as sticky

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ const GMDeployPage = ({
               </Suspense>
               <GMDeployTabs />
             </div>
-            <div className="w-[358px] px-5 py-6 border-x border-white/5 bg-white">
+            <div className="w-[358px] px-5 py-6 border-x border-white/5 bg-white sticky top-[102px] h-[calc(100vh-102px)]">
               <Suspense fallback={<div>Loading...</div>}>
                 <GMDeploySidebar />
               </Suspense>
